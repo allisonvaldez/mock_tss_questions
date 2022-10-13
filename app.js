33e84092bfd64ba74d46846312104e621467eeb2
Allison Valdez <ajsnipes@usfca.edu>
2022-10-13 15:34:12 -0400

    working on solution

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+
+4. Complete the 'onlyEvens' function below. 
+        The function is expected to return an INTEGER_ARRAY.
+        The function accepts INTEGER_ARRAY arr as parameter.
+*/
+
+console.log("arr1");
+
+function onlyEvens(arr) {
+    if (arr[i] % === 0) {
+        return arr[i];
+    }
+
+}
+
+console.log(onlyEvens([1,2,3,4,5]));
